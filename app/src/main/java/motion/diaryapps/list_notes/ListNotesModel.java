e0f5202182c75af2bb3d604656fbf1a0ceacbc66
yudhazulkarnaen <36037631+yudhazulkarnaen@users.noreply.github.com>
2019-04-23 21:04:02 +0700

    Update ListNotesModel.java

--- app/src/main/java/motion/diaryapps/list_notes/ListNotesModel.java
+++ app/src/main/java/motion/diaryapps/list_notes/ListNotesModel.java
@@ -10,14 +10,38 @@ public class ListNotesModel {
     private String id;
     private String image_url;
     // TODO: 4/12/19 -> Buat Attribute title dan date disini
+    private String title;
+    private String date;
     // hint: untuk date gunakan tipe data string, lakukan seperti contoh diatas
 
     // TODO: 4/12/19 -> Buat Constructor disini
+    public ListNotesModel(String id, String image_url, String title, String date) {
+        this.id = id;
+        this.image_url = image_url;
+        this.title = title;
+        this.date = date;
+    }
     // hint: Gunakan alt+insert
 
 
     // TODO: 4/12/19 -> Buat Getter dan Setternya disini
-    // hint: Gunakan alt+insert
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+// hint: Gunakan alt+insert
 
     public String getId() {
         return id;
